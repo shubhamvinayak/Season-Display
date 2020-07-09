903ed6db23d5f0e7f886ddaeffc09d3135bbc8d6
V <SV067337@cerner.net>
2020-07-09 11:52:23 +0530

    Updates

---
+++ precache-manifest.a29eb4a8cbdff8aa982e076d9d43d23e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a2b5ba0c950e101d344034136e2df083",
+    "url": "/Season-Display/index.html"
+  },
+  {
+    "revision": "4a5c446a712535ab2e8f",
+    "url": "/Season-Display/static/css/main.15376534.chunk.css"
+  },
+  {
+    "revision": "9b59cf3a204f0fb37124",
+    "url": "/Season-Display/static/js/2.89bf471f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Season-Display/static/js/2.89bf471f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4a5c446a712535ab2e8f",
+    "url": "/Season-Display/static/js/main.e020b8d6.chunk.js"
+  },
+  {
+    "revision": "2a1a055be0ab9c512c4c",
+    "url": "/Season-Display/static/js/runtime-main.0fa97c22.js"
+  }
+]);
